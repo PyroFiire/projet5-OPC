61e6874d808f045f6ad46a208dd22d9f1b3aa3d5
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-07-21 17:07:57 +0200

    add configExemple.php

---
+++ configExemple.php
@@ -0,0 +1,13 @@
+<?php
+
+$dataBase = [
+	'dbServer' => 'localhost:3308' ,
+	'dbName' => 'projet5-opc' ,
+	'dbUser' => 'root' ,
+	'dbPass' => ''
+];
+
+define("EMAIL_SMTP",    	"");
+define("PORT",     			"");
+define("EMAIL_ADDRESSE",	"");
+define("EMAIL_PASSWORD",	"");
