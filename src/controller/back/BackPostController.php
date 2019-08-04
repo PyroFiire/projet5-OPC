e8c67d475e68dce5d53df31697c4fa26b46bf1f1
Christophe Guinot <christophe.guinot@hotmail.fr>
2019-08-04 20:11:18 +0200

    verify the size of the forms data before insert/update

--- src/controller/back/BackPostController.php
+++ src/controller/back/BackPostController.php
@@ -6,20 +6,52 @@ class BackPostController extends SessionController{
 
 	public function addPost($postModel){
 
-		//The form is submitted, insert the post
-	    if(count($_POST)!==0){
+		//The form is not submitted, posting the post form
+    	if(count($_POST)===0){
+	        echo $this->twig->render('insertPost.twig', ['SESSION' => $_SESSION, 'add' => 'add']);
+	        return;
+    	}
 
-	    	$postModel->insertPost([	'title' => $_POST['title'],
-	    								'standfirst' => $_POST['standfirst'] ,
-	    								'contents' => $_POST['contents'] ,
-	    								'id_user' => $_SESSION['IdConnectedUser']
-	    							]);
+		//The form is submitted, control
+    	$title = (isset($_POST['title'])) ? $_POST['title'] : "" ;
+    	$standfirst = (isset($_POST['standfirst'])) ? $_POST['standfirst'] : "" ;
+		$contents = (isset($_POST['contents'])) ? $_POST['contents'] : "" ;
+		$id_user = (isset($_SESSION['IdConnectedUser'])) ? $_SESSION['IdConnectedUser'] : "" ;
+   		
+   		if (strlen($title)<1 || strlen($title)>100) {
+        $error['title'] ='Le titre n\'est pas renseigné ou invalide. Maximum 100 caractères';
+    	}
+    	if (strlen($standfirst)<1 || strlen($standfirst)>300) {
+        $error['standfirst'] ='Le chapô n\'est pas renseigné ou invalide. Maximum 300 caractères';
+    	}
+		if (strlen($contents)<1 || strlen($contents)>2000) {
+        $error['contents'] ='Le contenu n\'est pas renseigné ou invalide. Maximum 2000 caractères';
+    	}
+
+    	//insert post if control ok
+    	if (empty($error)) {
+	    	$postModel->insertPost([	
+	    		'title' => $title ,
+				'standfirst' => $standfirst ,
+				'contents' => $contents ,
+				'id_user' => $id_user
+			]);
+
 	    	$_SESSION['success'] = 'L\'article à bien été envoyé, il est maintenant en attente de validation par un administrateur';
 	    	header('location:Accueil');
 	    	exit;
-	    }
-	    //display the form
-		echo $this->twig->render('insertPost.twig', ['SESSION' => $_SESSION, 'add' => 'add']);
+        }
+
+		//form information in a table for simplicity with twig
+	    $form = [
+	    	'title' => $title ,
+	    	'standfirst' => $standfirst ,
+	    	'contents' => $contents ,
+	    	'id_user' => $id_user
+	    ];
+
+	    //display the form with errors and datas form
+		echo $this->twig->render('insertPost.twig', ['error' => $error, 'form' => $form, 'SESSION' => $_SESSION, 'add' => 'add']);
 	}
 
 	public function editPost($idPost,$postModel){
@@ -33,20 +65,49 @@ class BackPostController extends SessionController{
 	    	header('location:Articles-Page1');
 	    	exit;
 	    }
-		//The form is submitted, update the post
-	    if(count($_POST)!==0){
+	    //The form is not submitted, posting the post form
+    	if(count($_POST)===0){
+	        echo $this->twig->render('insertPost.twig', ['SESSION' => $_SESSION, 'post' => $post, 'edit'=> 'edit']);
+	        return;
+    	}
+
+    	//The form is submitted, control
+    	$title = (isset($_POST['title'])) ? $_POST['title'] : "" ;
+    	$standfirst = (isset($_POST['standfirst'])) ? $_POST['standfirst'] : "" ;
+		$contents = (isset($_POST['contents'])) ? $_POST['contents'] : "" ;
+
+   		if (strlen($title)<1 || strlen($title)>100) {
+        $error['title'] ='Le titre n\'est pas renseigné ou invalide. Maximum 100 caractères';
+    	}
+    	if (strlen($standfirst)<1 || strlen($standfirst)>300) {
+        $error['standfirst'] ='Le chapô n\'est pas renseigné ou invalide. Maximum 300 caractères';
+    	}
+		if (strlen($contents)<1 || strlen($contents)>2000) {
+        $error['contents'] ='Le contenu n\'est pas renseigné ou invalide. Maximum 2000 caractères';
+    	}
+
+		//if no error, update the post
+	    if(empty($error)){
 	    	$postModel->updatePost($idPost,[
-	    		'title' => $_POST['title'],
-	    		'standfirst' => $_POST['standfirst'],
-	    		'contents' => $_POST['contents']
+	    		'title' => $title,
+	    		'standfirst' => $standfirst,
+	    		'contents' => $contents
 	    		]);
 	    	$_SESSION['success'] = 'L\'article à été mis à jour';
 	    	header('location:Article-'.$post['id'].'-page1');
 	    	exit;
 	    }
 
-	    //display the post
-		echo $this->twig->render('insertPost.twig', ['SESSION' => $_SESSION, 'post' => $post, 'edit'=> 'edit']);
+	    //form information in a table for simplicity with twig
+	    $form = [
+	    	'title' => $title ,
+	    	'standfirst' => $standfirst ,
+	    	'contents' => $contents ,
+	    	'id_post' => $idPost
+	    ];
+
+	    //display the post with error and datas form
+		echo $this->twig->render('insertPost.twig', ['error' => $error, 'SESSION' => $_SESSION, 'form' => $form, 'edit'=> 'edit']);
 	}
 
 	public function deletePost($idPost,$postModel){
--- src/controller/back/CommentController.php
+++ src/controller/back/CommentController.php
@@ -7,11 +7,24 @@ class CommentController extends SessionController{
 
 	public function insertComment($postModel,$commentModel, $idPost){
 
-		//The form is submitted, insert the comment and redirect
-	    if(count($_POST)!==0){
+		//The form is not submitted, exit
+    	if(count($_POST)===0){
+	    	$_SESSION['error'] = 'Formulaire non soumis';
+	        header('location:Article-'. $idPost .'-Page1');
+	    	exit;
+    	}
 
+		//The form is submitted, control
+    	$contents = (isset($_POST['contents'])) ? $_POST['contents'] : "" ;
+
+    	if (strlen($contents)<1 || strlen($contents)>1000) {
+        $error['contents'] ='Le commentaire n\'est pas renseigné ou invalide. Maximum 1000 caractères';
+    	}
+
+    	//insert the comment and redirect
+    	if (empty($error)){
 	    	$commentModel->insertComment([	
-	    		'contents' => $_POST['contents'],
+	    		'contents' => $contents,
 	    		'id_blog_post' => $idPost ,
 	    		'id_user' => $_SESSION['IdConnectedUser']
 	    	]);
@@ -19,6 +32,12 @@ class CommentController extends SessionController{
 	    	$_SESSION['success'] = 'Votre commentaire à été envoyé, il est en attente de validation par un administrateur';
 	    	header('location:Article-'. $idPost .'-Page1');
 	    	exit;
-	    }
+	    } else {
+	    	//redirect on post with error
+	    	$_SESSION['error'] = $error['contents'];
+	    	header('location:Article-'. $idPost .'-Page1');
+	    	exit;
+	    	
+		}
 	}
 }
